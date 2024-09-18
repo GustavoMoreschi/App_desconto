1c0e2414d3dc5c6ef0bead3a2f60b7b9c2e52b60
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 11:29:50 -0300

    Adicionando projeto ao github

---
+++ app/src/main/java/br/com/gustavomoreschi/descontofinal/DiscountModel.kt
@@ -0,0 +1,13 @@
+package br.com.gustavomoreschi.descontofinal
+
+data class DiscountModel(
+    val price: Double,
+    val discountPercentage: Double
+) {
+    fun calculateDiscount(): Double {
+        return price * discountPercentage / 100
+    }
+    fun calculateTotalPrice(): Double {
+        return price - calculateDiscount()
+    }
+}
